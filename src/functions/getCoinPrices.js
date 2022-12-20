2bda1831e7b866c1376ef750fe5c289c3ac361b0
Abhay <abhaysinghparmar123@gmail.com>
2022-12-20 22:45:10 +0530

    netlify host

--- src/functions/getCoinPrices.js
+++ src/functions/getCoinPrices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const getCoinPrices = (id,days) => {
+export const getCoinPrices = (id,days,priceType) => {
    const prices =  axios
       .get(
         `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=${days}&interval=daily`
